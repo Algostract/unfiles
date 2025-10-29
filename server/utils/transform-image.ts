c761da0d2c36219cf0567cdde6bc7b477c59bed9
Shirsendu Bairagi <shirsendu2001@gmail.com>
2025-10-29 17:23:00 +0530

    refactor: remove ipx dependency and transformed image transformation logic to ipx server

--- server/utils/transform-image.ts
+++ server/utils/transform-image.ts
@@ -1,66 +1,45 @@
 import { consola } from 'consola'
 import PQueue from 'p-queue'
-import { createIPX, ipxFSStorage, ipxHttpStorage } from 'ipx'
-import { contentType as lookup } from 'mime-types'
-
-const ipx = createIPX({
-  storage: ipxFSStorage({ dir: './static' }),
-  httpStorage: ipxHttpStorage({ allowAllDomains: true }),
-})
 
 const queue = new PQueue({
   intervalCap: parseInt(process.env.RATE_LIMIT_INTERVAL_CAP || '5'),
   interval: parseInt(process.env.RATE_LIMIT_INTERVAL || '1000'),
 })
 
-function toArrayBuffer(input: Buffer | Uint8Array | ArrayBuffer): ArrayBuffer {
-  if (input instanceof ArrayBuffer) return input
-  const u8 = Buffer.isBuffer(input) ? new Uint8Array(input.buffer, input.byteOffset, input.byteLength) : input
-
-  return u8.buffer.slice(u8.byteOffset, u8.byteOffset + u8.byteLength) as ArrayBuffer
-}
-
 const transform = (cacheKey: string, mappedSource: string, modifiers: Record<string, string | number | boolean>) =>
   queue.add(async () => {
     const source = `${process.env.NUXT_PRIVATE_CLOUDREVE_R2_PUBLIC_URL}/${encodeURI(mappedSource)}`
-    // consola.log('🛠️ Transform START', { source, modifiers })
+    const ipxUrl = `${process.env.INTERNAL_BASE_URL}/_ipx/${stringifyIpxArgs(modifiers)}/${source}`
 
-    const { data } = await ipx(source, modifiers).process()
-    if (typeof data === 'string') {
-      throw createError({ statusCode: 500, message: 'data is a string' })
+    const res = await fetch(ipxUrl, { method: 'GET' })
+    if (!res.ok || !res.body) {
+      console.log({ ipxUrl }, '🚧 Data is undefined')
+      throw createError({ statusCode: 404, message: '🚧 Data is undefined' })
     }
 
-    const buffer = toArrayBuffer(data as Buffer | Uint8Array | ArrayBuffer)
-
-    const mime = (typeof modifiers.format === 'string' && (lookup(modifiers.format) || undefined)) || 'application/octet-stream'
-
     // consola.log('📦 Transform DONE', { cacheKey, bytes: buffer.byteLength })
-    const dataStream = new Response(buffer).body!
-    const [toDisk, toR2] = dataStream.tee()
+    const byteLength = parseInt(res.headers.get('content-length') ?? '0')
+    const [toDisk, toR2] = res.body.tee()
     const diskCacheKey = `./static/${cacheKey}`
 
     // Cache to Storage
 
-    r2PutFileStream(cacheKey, toR2, data.byteLength)
+    r2PutFileStream(cacheKey, toR2, byteLength)
       .then(() => {
-        consola.info('💾 Saved to R2 cache', { cacheKey, bytes: data.byteLength })
+        consola.info('💾 Saved to R2 cache', { cacheKey, bytes: byteLength })
       })
       .then(() => diskPutFileStream(diskCacheKey, toDisk))
       .then(() => {
-        consola.info('💾 Saved to FS cache', { cacheKey, bytes: data.byteLength })
+        consola.info('💾 Saved to FS cache', { cacheKey, bytes: byteLength })
       })
       .catch((error) => {
         consola.error('Failed to save to cache', error)
       })
 
     return {
-      stream: new Response(buffer).body!,
-      byteLength: buffer.byteLength,
-      contentType: mime,
-    } as {
-      stream: ReadableStream
-      byteLength: number
-      contentType: string
+      stream: res.body,
+      byteLength: parseInt(res.headers.get('content-length') ?? '0'),
+      contentType: res.headers.get('content-type') ?? undefined,
     }
   })
 
